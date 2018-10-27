bd18170b3ee3224c843f8d6dedb6be5ffd3690c6
Dr. Lev Avri <avrilev@windowslive.com>
2018-10-27 09:30:57 +0300

    Add project files.

---
+++ PolymorphismC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolymorphismC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
