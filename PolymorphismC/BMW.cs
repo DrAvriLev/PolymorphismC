be6a8bbfb8112d1e7071624620fedf460bdc681a
Dr. Lev Avri <avrilev@windowslive.com>
2018-10-27 10:23:20 +0300

    Polymorphism

---
+++ PolymorphismC/BMW.cs
@@ -0,0 +1,13 @@
+﻿namespace PolymorphismC
+{
+    public class BMW : Car
+    {
+        public string Model { get; set; }
+
+        public BMW(int hp, string color, string model) :
+            base(hp, color)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ PolymorphismC/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PolymorphismC
+{
+    public class Car
+    {
+        //private int hp;
+        //private string color;
+
+        public int HP { get; set; }
+        public string Color { get; set; }
+
+        public Car(int hp, string color)
+        {
+            HP = hp;
+            Color = color;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"The car's HP is {HP} and it's color is {Color}. ");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine("Car is repaired.");
+        }
+    }
+}
